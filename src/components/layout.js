d8048f937f26606d7da077d5d140c4ac4855bd34
RicardoQuirino <ricardoquirino72@gmail.com>
2020-11-19 15:42:54 -0300

    Primeiro comit

---
+++ src/components/layout.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./header"
+import { GlobalStyle} from './styles/GlobalStyles'
+
+const Layout = ( { children } ) => {
+  return (
+    <>
+      <GlobalStyle/>
+      <Header />
+      <main>{children}</main>
+    </>
+  )
+}
+export default Layout
